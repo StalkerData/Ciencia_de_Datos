6fd230fa33a8ea50029b4e90546baaf3973efecc
StalkerData <marlon.duquemar@unipamplona.edu.co>
2026-01-14 13:51:01 -0500

    Función para crear un gráfico y guardarlo en el repositorio results para las gráficas de barras comparando lo teórico y simulado.

--- Nivel_1/lanzamiento_dados/src/simulador.py
+++ Nivel_1/lanzamiento_dados/src/simulador.py
@@ -1,11 +1,14 @@
 import numpy as np
 
+
 class Simulador:
     @classmethod
     def dados(cls, caras_dado, cantidad_dados=1):
         cls.caras_dado = caras_dado
-        cls.cantidad_dados=cantidad_dados
-    
+        cls.cantidad_dados = cantidad_dados
+
     @classmethod
     def lanzamientos(cls, cantidad_lanzamientos=1000000):
-        return np.random.randint(1,cls.caras_dado+1,size=(cantidad_lanzamientos,cls.cantidad_dados))
+        return np.random.randint(
+            1, cls.caras_dado + 1, size=(cantidad_lanzamientos, cls.cantidad_dados)
+        )
